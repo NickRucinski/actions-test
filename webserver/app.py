50dcf30a24e9f373420d0b544fe397f9efe1e2f3
HydroLink <nrucinski01@gmail.com>
2025-02-21 23:37:09 -0500

    Swagger definition for post log route

--- webserver/app.py
+++ webserver/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from routes import register_blueprints
+from flasgger import Swagger
 
 port = 8001
 ip_address = "0.0.0.0"
@@ -8,6 +9,7 @@ ip_address = "0.0.0.0"
 app = Flask(__name__)
 CORS(app)
 register_blueprints(app)
+swagger = Swagger(app)
 
 if __name__ == '__main__':
     app.run(host=ip_address, port=port, debug=True)
--- webserver/routes/logging.py
+++ webserver/routes/logging.py
@@ -1,10 +1,56 @@
 from flask import request, jsonify, Blueprint
 from database import log_event
-import time
+from flasgger import swag_from
 
 logging_bp = Blueprint('logging', __name__)
 
 @logging_bp.route('/log', methods=['POST'])
+@swag_from({
+    'tags': ['Logging'],
+    'summary': 'Log an event',
+    'description': 'Receives an event and logs the event.',
+    'parameters': [
+        {
+            'name': 'body',
+            'in': 'body',
+            'required': True,
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'timestamp': {
+                        'type': 'string',
+                        'example': 1708401940
+                        },
+                    'event': {
+                        'type': 'string',
+                        'example': 'User logged in'
+                        },
+                    'data': {
+                        'type': 'integer',
+                        'example': {
+                            'userID': 12345,
+                            }
+                        }
+                },
+                'required': ['message', 'timestamp']
+            }
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': 'Event logged successfully',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'status': {'type': 'string', 'example': 'logged'}
+                }
+            }
+        },
+        '400': {
+            'description': 'Invalid input'
+        }
+    }
+})
 def log_event_route():
     data = request.json
     log_event(data)
