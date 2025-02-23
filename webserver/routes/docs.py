09532e0ebf10757cda4a36b47565a8aceb564b80
HydroLink <nrucinski01@gmail.com>
2025-02-23 00:01:43 -0500

    added endpoints for the TypeDoc docs

--- webserver/routes/docs.py
+++ webserver/routes/docs.py
@@ -4,6 +4,7 @@ import os
 docs_bp = Blueprint('docs', __name__)
 
 docs_path = os.path.join(os.getcwd(), 'docs/build/html')
+tsdocs_path = os.path.join(os.getcwd(), 'docs/tsdoc')
 
 @docs_bp.route('/docs/<path:filename>')
 def serve_docs(filename):
@@ -25,3 +26,24 @@ def serve_static(filename):
     Serves static assets for Sphinx documentation
     """
     return send_from_directory(os.path.join(docs_path, '_static'), filename)
+
+@docs_bp.route('/tsdocs/<path:filename>')
+def serve_tsdocs(filename):
+    """
+    Serves the TypeDoc-generated HTML documentation.
+    """
+    return send_from_directory(tsdocs_path, filename)
+
+@docs_bp.route('/tsdocs/')
+def docs_tsindex():
+    """
+    Redirects to the main TypeDoc documentation page.
+    """
+    return send_from_directory(tsdocs_path, "index.html")
+
+@docs_bp.route('/tsdocs/assets/<path:filename>')
+def serve_tsstatic(filename):
+    """
+    Serves static assets for TypeDoc documentation
+    """
+    return send_from_directory(os.path.join(tsdocs_path, 'assets'), filename)
